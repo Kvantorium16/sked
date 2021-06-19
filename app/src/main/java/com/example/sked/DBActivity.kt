1f40cc5f6b8a4035d503f46eed8778ea7df5b76f
Fatima <fatimaserikova0@gmail.com>
2021-06-19 15:11:24 +0600

    Перенесла всё на новый activity. Добавила переход на dbactivity при долгом нажатии на кнопку "назад".

---
+++ app/src/main/java/com/example/sked/DBActivity.kt
@@ -0,0 +1,51 @@
+package com.example.sked
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_dbactivity.*
+
+class DBActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dbactivity)
+
+        val context = this
+        var db = DataBaseHandler(context)
+        btn_save.setOnClickListener {
+            if (subject.text.toString().isNotEmpty() &&
+                date.text.toString().isNotEmpty() &&
+                time.text.toString().isNotEmpty() &&
+                teacher.text.toString().isNotEmpty()
+            ) {
+
+                var subject = Subject(
+                    subject.text.toString(), date.text.toString(),
+                    time.text.toString(), teacher.text.toString()
+                )
+
+                db.insertData(subject)
+
+            } else {
+                Toast.makeText(context,"Пожалуйства заполните всю информацию", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btn_read.setOnClickListener {
+            var data = db.readData()
+            result.text = ""
+            for (i in 0 until data.size-1) {
+                result.append(
+                    "\n№" + data[i].id.toString() + ". Subject:" + data[i].subject + " Date:" + data[i].date + " Time:"
+                            + data[i].time + " Teacher:" + data[i].teacher
+                )
+            }
+        }
+
+        btn_delete.setOnClickListener {
+            db.deleteData()
+            btn_read.performClick()
+        }
+
+    }
+}
--- app/src/main/java/com/example/sked/MainActivity.kt
+++ app/src/main/java/com/example/sked/MainActivity.kt
@@ -1,52 +1,24 @@
 
 package com.example.sked
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import android.view.KeyEvent
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val context = this
-        var db = DataBaseHandler(context)
-    btn_save.setOnClickListener {
-        if (subject.text.toString().isNotEmpty() &&
-            date.text.toString().isNotEmpty() &&
-            time.text.toString().isNotEmpty() &&
-            teacher.text.toString().isNotEmpty()
-        ) {
-
-            var subject = Subject(
-                subject.text.toString(), date.text.toString(),
-                time.text.toString(), teacher.text.toString()
-            )
-
-            db.insertData(subject)
-
-        } else {
-            Toast.makeText(context,"Пожалуйства заполните всю информацию", Toast.LENGTH_SHORT).show()
-        }
     }
 
-        btn_read.setOnClickListener {
-            var data = db.readData()
-            result.text = ""
-            for (i in 0 until data.size-1) {
-                result.append(
-                    "\n№" + data[i].id.toString() + ". Subject:" + data[i].subject + " Date:" + data[i].date + " Time:"
-                            + data[i].time + " Teacher:" + data[i].teacher
-                )
-            }
+    override fun onKeyLongPress(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            val intent = Intent(this, DBActivity::class.java)
+            startActivity(intent)
+            println("Back button long pressed");
+            return true;
         }
-
-        btn_delete.setOnClickListener {
-            db.deleteData()
-            btn_read.performClick()
-        }
-
+        return super.onKeyLongPress(keyCode, event)
     }
 }
